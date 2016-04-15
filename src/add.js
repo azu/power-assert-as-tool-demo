026cfc21f49f980a2726e303a32edbae2efbd1ab
azu <azuciao@gmail.com>
2016-04-15 22:26:39 +0900

    feat(babel): add unassert in NODE_ENV=production building

--- src/add.js
+++ src/add.js
@@ -1,9 +1,12 @@
 "use strict";
+const assert = require("assert");
 /**
- * @param {number} x
- * @param {number} y
+ * @param {number} x x is integer
+ * @param {number} y y is integer
  * @returns {Number}
  */
 export default function add(x, y) {
+    assert(Number.isInteger(x));
+    assert(Number.isInteger(y));
     return x + y;
 }
